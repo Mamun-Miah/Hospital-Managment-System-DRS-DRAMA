778e128851eb22db18916ec9f8176b0ccdad8a21
Mamun-Miah <mahadyhasen7@gmail.com>
2025-07-10 17:22:13 +0600

    Prescribed 4

--- src/app/(admin)/doctor/prescriptions/[id]/page.tsx
+++ src/app/(admin)/doctor/prescriptions/[id]/page.tsx
@@ -363,6 +363,22 @@ const AddAppointment: React.FC = () => {
                 className="h-[55px] rounded-md text-black dark:text-white border border-gray-200 dark:border-[#172036] bg-gray-100 dark:bg-[#0c1427] px-[17px] block w-full outline-0 transition-all placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:border-primary-500"
               />
             </div>
+
+             <div>
+              <label className="mb-[10px] text-black dark:text-white font-medium block">
+                Treatment Duration
+              </label>
+              <input
+                type="text"
+                value={
+                  formData.treatmentDuration +
+                  (formData.treatmentDuration > 1 ? " Months" : " Month")
+                }
+                disabled
+                className="h-[55px] rounded-md text-black dark:text-white border border-gray-200 dark:border-[#172036] bg-gray-100 dark:bg-[#0c1427] px-[17px] block w-full outline-0 transition-all placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:border-primary-500"
+              />
+            </div>     
+
             <div>
               <label className="mb-[10px] text-black dark:text-white font-medium block">
                 Doctor Fees
@@ -405,20 +421,7 @@ const AddAppointment: React.FC = () => {
                 className="h-[55px] rounded-md text-black dark:text-white border border-gray-200 dark:border-[#172036] bg-white dark:bg-[#0c1427] px-[17px] block w-full outline-0 transition-all placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:border-primary-500 show-spinner"
               />
             </div>
-            <div>
-              <label className="mb-[10px] text-black dark:text-white font-medium block">
-                Treatment Duration
-              </label>
-              <input
-                type="text"
-                value={
-                  formData.treatmentDuration +
-                  (formData.treatmentDuration > 1 ? " Months" : " Month")
-                }
-                disabled
-                className="h-[55px] rounded-md text-black dark:text-white border border-gray-200 dark:border-[#172036] bg-gray-100 dark:bg-[#0c1427] px-[17px] block w-full outline-0 transition-all placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:border-primary-500"
-              />
-            </div>
+            
           </div>
         </div>
 
