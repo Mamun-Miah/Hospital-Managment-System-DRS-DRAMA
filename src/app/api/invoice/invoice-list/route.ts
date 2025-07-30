94f5e2ed4423f12b14cc549f3fcf1929ed6fbd22
Mamun-Miah <mahadyhasen7@gmail.com>
2025-07-30 12:18:39 +0600

    new column added invoice_id in the Prescription Table

---
+++ src/app/api/invoice/invoice-list/route.ts
@@ -0,0 +1,56 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const prescriptionList = await prisma.prescription.findMany({
+  where: {
+    is_prescribed: "Yes",
+  },
+  select: {
+    prescription_id: true,
+    prescribed_at: true,
+    is_prescribed: true,
+    is_drs_derma: true,
+    patient_id: true,
+    patient: {
+      select: {
+        patient_name: true,
+        mobile_number:true,
+      },
+    },
+    doctor_id: true,
+    doctor: {
+      select: {
+        doctor_name: true,
+      },
+    },
+  },
+});
+
+
+ const prescriptionListData = prescriptionList.map((item) => ({
+      prescription_id: item.prescription_id,
+      prescribed_at: item.prescribed_at.toISOString().slice(0, 10),
+      is_prescribed: item.is_prescribed,
+      is_drs_derma: item.is_drs_derma,
+      patient_id: item.patient_id,
+      patient_name: item.patient?.patient_name ?? "",
+      mobile_number: item.patient?.mobile_number ?? "",
+      doctor_id: item.doctor_id,
+      doctor_name: item.doctor?.doctor_name ?? "",
+    }));
+
+
+
+
+
+    return NextResponse.json({ prescriptionlist: prescriptionListData });
+  } catch (error) {
+    console.error("Error fetching prescriptions:", error);
+    return NextResponse.json(
+      { error: "An error occurred while fetching prescriptions." },
+      { status: 500 }
+    );
+  }
+}
