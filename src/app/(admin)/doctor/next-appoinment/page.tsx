01d22e6fac06f37278540300defbcccd7b8d1980
Mamun-Miah <mahadyhasen7@gmail.com>
2025-08-04 16:00:03 +0600

    next_appoinment_list_created2

--- src/app/(admin)/doctor/next-appoinment/page.tsx
+++ src/app/(admin)/doctor/next-appoinment/page.tsx
@@ -212,9 +212,7 @@ const NextAppointmentList: React.FC = () => {
             </Link>
           </li>
 
-          <li className="breadcrumb-item inline-block relative text-sm mx-[11px] ltr:first:ml-0 rtl:first:mr-0 ltr:last:mr-0 rtl:last:ml-0">
-            Doctor
-          </li>
+         
 
           <li className="breadcrumb-item inline-block relative text-sm mx-[11px] ltr:first:ml-0 rtl:first:mr-0 ltr:last:mr-0 rtl:last:ml-0">
             Next Appointment List
