4e879425b9128afb25b3d5a5922d1adc9327b7bf
Mamun-Miah <mahadyhasen7@gmail.com>
2025-08-07 14:40:30 +0600

    Patient history update3

--- src/components/Layout/SidebarMenu/index.tsx
+++ src/components/Layout/SidebarMenu/index.tsx
@@ -3320,7 +3320,7 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ toggleActive }) => {
                       </Link>
                     </li>
 
-                    <li className="sidemenu-item mb-[4px] last:mb-0">
+                    {/* <li className="sidemenu-item mb-[4px] last:mb-0">
                       <Link
                         href="/settings/connections/"
                         className={`sidemenu-link rounded-md flex items-center relative transition-all font-medium text-gray-500 dark:text-gray-400 py-[9px] ltr:pl-[38px] ltr:pr-[30px] rtl:pr-[38px] rtl:pl-[30px] hover:text-primary-500 hover:bg-primary-50 w-full text-left dark:hover:bg-[#15203c] ${
@@ -3329,9 +3329,9 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ toggleActive }) => {
                       >
                         Connections
                       </Link>
-                    </li>
+                    </li> */}
 
-                    <li className="sidemenu-item mb-[4px] last:mb-0">
+                    {/* <li className="sidemenu-item mb-[4px] last:mb-0">
                       <Link
                         href="/settings/privacy-policy/"
                         className={`sidemenu-link rounded-md flex items-center relative transition-all font-medium text-gray-500 dark:text-gray-400 py-[9px] ltr:pl-[38px] ltr:pr-[30px] rtl:pr-[38px] rtl:pl-[30px] hover:text-primary-500 hover:bg-primary-50 w-full text-left dark:hover:bg-[#15203c] ${
@@ -3355,7 +3355,7 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ toggleActive }) => {
                       >
                         Terms & Conditions
                       </Link>
-                    </li>
+                    </li> */}
                   </ul>
                 </div>
               </div>
