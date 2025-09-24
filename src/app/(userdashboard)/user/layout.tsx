edf6bd33aebdc6a94c475cb76313bf12365ea113
Mamun-Miah <mahadyhasen7@gmail.com>
2025-09-24 11:52:15 +0600

    updated metadata

--- src/app/(userdashboard)/user/layout.tsx
+++ src/app/(userdashboard)/user/layout.tsx
@@ -1,7 +1,11 @@
 // app/layout.tsx
 import "../../(hmsdashboard)/globals.css";
 import ProtectedLayoutClient from "./ProtectedLayoutClient";
-
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "DRS DERMA - User Dashboard",
+  description: "DRS DERMA User Dashboard",
+};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
